4b4fd0b00ce00b32e5cb3686143edecc14b5e916
camilaezama <camilaezama2000@gmail.com>
2023-12-28 16:45:32 -0300

    Pruebas Conectarse API

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -3,6 +3,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'dart:typed_data'; // Para Uint8List
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,22 +12,23 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<List<dynamic>> csvData = [];
+  String boton = 'Aceptar';
 
   void _loadCSVData(FilePickerResult result) async {
-      Uint8List? fileBytes = result.files.first.bytes;
-      String fileName = result.files.first.name;
+    Uint8List? fileBytes = result.files.first.bytes;
+    String fileName = result.files.first.name;
 
-      if (fileName.endsWith('.csv')) {
-        String fileContent = utf8.decode(fileBytes!);
-        List<List<dynamic>> csvTable =
-            const CsvToListConverter().convert(fileContent);
+    if (fileName.endsWith('.csv')) {
+      String fileContent = utf8.decode(fileBytes!);
+      List<List<dynamic>> csvTable =
+          const CsvToListConverter().convert(fileContent);
 
-        setState(() {
-          csvData = csvTable;
-        });
-      } else {
-        print('Invalid file type. Please select a CSV file.');
-      }
+      setState(() {
+        csvData = csvTable;
+      });
+    } else {
+      print('Invalid file type. Please select a CSV file.');
+    }
   }
 
   void _selectFile() async {
@@ -148,8 +150,41 @@ class _HomePageState extends State<HomePage> {
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/grillas');
+                    onPressed: () async {
+                      //Navigator.pushNamed(context, '/grillas');
+
+                      ByteData byteData = csvToByteData(csvData);
+
+                      String content =
+                          String.fromCharCodes(byteData.buffer.asUint8List());
+
+                      // Parsear el contenido CSV con el delimitador ';'
+                      List<List<dynamic>> rows = const CsvToListConverter(
+                              eol: '\n', fieldDelimiter: ';')
+                          .convert(content);
+
+                      //print(rows);
+
+                      List<Map<String, String>> result = convertRowsToMap(rows);
+
+                      //print(result);
+
+                      String jsonResult = jsonEncode(result);
+
+                      print(jsonResult);
+
+                      try {
+                        var url = Uri.parse('http://localhost:7777');
+                        var response = await http.post(url, body: jsonResult);
+
+                        print('Response status: ${response.statusCode}');
+                        print('Response body: ${response.body}');
+                        setState(() {
+                          boton = 'La respuesta fue: ${response.body}';
+                        });
+                      } catch (e) {
+                        print('Error: $e');
+                      }
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
@@ -159,8 +194,8 @@ class _HomePageState extends State<HomePage> {
                         borderRadius: BorderRadius.circular(8.0),
                       ),
                     ),
-                    child: const Text(
-                      'Aceptar',
+                    child: Text(
+                      boton,
                       style: TextStyle(fontSize: 16),
                     ),
                   ),
@@ -172,5 +207,45 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
-  
+
+  ByteData csvToByteData(List<List<dynamic>> csvTable) {
+    // Supongamos que tienes una función que convierte tu tabla CSV a una lista de bytes
+    List<int> byteList = convertCsvTableToBytes(csvTable);
+
+    // Convertir la lista de bytes a Uint8List
+    Uint8List uint8List = Uint8List.fromList(byteList);
+
+    // Crear ByteData a partir de Uint8List
+    ByteData byteData = uint8List.buffer.asByteData();
+
+    return byteData;
+  }
+
+  List<int> convertCsvTableToBytes(List<List<dynamic>> csvTable) {
+    // Aquí debes implementar la lógica para convertir tu tabla CSV a una lista de bytes
+    // Puedes usar 'utf8.encode' si tu CSV está en formato de texto
+    // Por ejemplo, puedes hacer algo como:
+    String csvString = const ListToCsvConverter().convert(csvTable);
+    List<int> byteList = utf8.encode(csvString);
+
+    return byteList;
+  }
+
+  List<Map<String, String>> convertRowsToMap(List<List<dynamic>> rows) {
+    List<String> columnNames = List.from(rows[0]);
+    List<Map<String, String>> result = [];
+
+    for (int i = 1; i < rows.length; i++) {
+      Map<String, String> rowMap = {};
+
+      for (int j = 0; j < columnNames.length; j++) {
+        // Convertir el valor a String y asignarlo al mapa con el nombre de la columna
+        rowMap[columnNames[j]] = rows[i][j].toString();
+      }
+
+      result.add(rowMap);
+    }
+
+    return result;
+  }
 }
