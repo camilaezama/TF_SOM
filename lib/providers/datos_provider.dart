23b338b2e44e351693233b15c85dad55078d7672
Augusto <65096407+augustomtt@users.noreply.github.com>
2024-05-29 12:05:23 -0300

    Procesamiento de los labels, primera implementacion

--- lib/providers/datos_provider.dart
+++ lib/providers/datos_provider.dart
@@ -40,8 +40,10 @@ class DatosProvider extends ChangeNotifier {
     List<dynamic> UmatJSON = decodedJson["UMat"];
 
     Map<String, dynamic> HitsJSON = decodedJson["Hits"];
+    Map<String, dynamic> HitsLabelsJSON = decodedJson["HitsLabels"];
     
     var tempHitsMap = procesarHits(HitsJSON);
+    //var tempHitsLabelsMap = null;// procesarHitsLabels(HitsLabelsJSON);
     var tempMapaRta = procesarBmus(NeuronsJSON, tipoLlamada);
     var tempDataUdist = tempMapaRta["Udist"]!;
     var tempMapaRtaUmat = procesarUmat(UmatJSON)!;
@@ -68,6 +70,13 @@ class DatosProvider extends ChangeNotifier {
     Map<int, int> hitsMap = Map<int, int>.from(
         HitsJSON.map((key, value) => MapEntry(int.parse(key), value)));
     return hitsMap;
+  }
+
+ Map<int, List> procesarHitsLabels(Map<String, dynamic> HitsLabelsJSON) {
+    Map<int, List> hitsLabelsMap = Map<int, List>.from(
+        HitsLabelsJSON.map((key, value) => MapEntry(int.parse(key), value)));
+    print(hitsLabelsMap);
+    return hitsLabelsMap;
   }
 
   Map<String, String>? procesarUmat(List<dynamic> UmatJSON) {
