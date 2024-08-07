7971a94d0d9afee0ec03da145db77b757619865d
camilaezama <camilaezama2000@gmail.com>
2024-08-07 19:06:25 -0300

    Cambios visuales e icono

--- lib/presentacion/pages/menu_grillas_page.dart
+++ lib/presentacion/pages/menu_grillas_page.dart
@@ -85,21 +85,25 @@ class _GrillasPageState extends State<GrillasPage>
     return Scaffold(
       appBar: AppBar(
         actions: [
+          /// BOTON DE INFORMACION
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
-              child: Icon(Icons.info),
-              onPressed: () {
-                showDialog<void>(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return InfoErroresDialog(
-                      widthPantalla: _width,
-                      heightPantalla: _height,
-                    );
-                  },
-                );
-              },
+            child: Tooltip(
+              message: 'Información',
+              child: ElevatedButton(
+                child: Icon(Icons.info),
+                onPressed: () {
+                  showDialog<void>(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return InfoErroresDialog(
+                        widthPantalla: _width,
+                        heightPantalla: _height,
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ),
         ],
@@ -111,7 +115,7 @@ class _GrillasPageState extends State<GrillasPage>
           children: [
             DrawerHeader(
                 decoration: BoxDecoration(
-                  color: Colors.blue,
+                  color: Theme.of(context).colorScheme.secondary,
                 ),
                 child: Column(
                   children: [
